7e9f31dcb9dadad6af95486b0925a29cb06763e2
mariastlouis <msantray@yahoo.com>
2018-01-22 15:13:03 -0700

    Initial commit

---
+++ public/js/scripts.js
@@ -0,0 +1,14 @@
+
+const generatePalette = () => {
+   var letters = '0123456789ABCDEF';
+   var color = '#';
+  for(var i = 0; i< 6; i++ ) {
+    color += letters[Math.floor(Math.random() *16)];
+  }
+  return color
+}
+
+
+
+$('.generate-btn').on('click', generatePalette)
+
