15d950bdf8ac78d147e982896fb29dea6ccc480b
mariastlouis <msantray@yahoo.com>
2018-01-24 15:56:40 -0700

    all get fetch requests passing

--- server.js
+++ server.js
@@ -62,3 +62,66 @@ app.post('/api/v1/projects', (request, response) => {
       return response.status(500).json({error});
     })
 });
+
+app.post('/api/v1/projects/:projectId/palettes', (request, response) =>{
+  const {projectId} = request.params;
+  const palette = Object.assign({}, request.body, { project_id: projectId});
+  for (let requiredParameter of ['title', 'color1', 'color2', 'color3', 'color4', 'color5']) {
+    if(!palette[requiredParameter]){
+      return response.status(422).json({
+        error: `You are missing the required parameter ${requiredParameter}`
+      })
+    }
+  }
+  database('palettes').insert(palette, 'id')
+  .then(project => {
+    return response.status(201).json({id: project[0]})
+  })
+  .catch(error => {
+    return response.status(500).json({error})
+  })
+});
+
+
+app.get('/api/v1/projects/:id', (request, response) =>{
+  database('projects').where('id', request.params.id).select()
+  .then(projects => {
+    if(projects.length) {
+      return response.status(200).json({projects})
+    } else {
+      return response.status(404).json({
+        error: `Could not find project with id of ${request.params.id}`
+      })
+    }
+  })
+  .catch(error => {
+    return response.status(500).json({error})
+  })
+})
+
+app.get('/api/v1/projects/:projectId/palettes', (request, response) =>{
+  const {projectId} = request.params;
+  database('palettes').where('project_id', projectId).select()
+  .then(palettes =>{
+    return response.status(200).json({palettes})
+  })
+  .catch(error => {
+    return response.status(500).json({error})
+  })
+})
+
+app.get('/api/v1/palettes/:id', (request, response) =>{
+  database('palettes').where('id', request.params.id).select()
+  .then(palettes => {
+    if(palettes.length) {
+      return response.status(200).json({palettes})
+    } else {
+      return response.status(404).json({
+        error: `Could not find project with id of ${request.params.id}`
+      })
+    }
+  })
+  .catch(error => {
+    return response.status(500).json({error})
+  })
+})
