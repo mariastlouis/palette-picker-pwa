8d0525880e5c4879bbce51510361bb1afee58af2
mariastlouis <msantray@yahoo.com>
2018-01-24 14:50:18 -0700

    back end set up with seeds

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -4,14 +4,26 @@ $(document).ready(() => {
 });
 
 const setColors = () => {
+  reloadAnimation()
   const colorNumbers = [1, 2, 3, 4, 5];
   colorNumbers.forEach(number => {
     if(!$(`.color-box${number}`).hasClass('favorite')) {
     let color = generateColors();
+
+
+    $(`.circle${number}`).css("background-color", color)
     $(`.color-box${number}`).css("background-color", color)
     $(`.hex-code${number}`).text(color.toUpperCase())
+    $(`.hex-code${number}`).css("color", darkenColor)
   }
   });
+}
+
+const reloadAnimation = () => {
+  const donut = $('.donut-chart-article')
+  const newDonut = donut.clone(true);
+  donut.before(newDonut);
+  $("." + donut.attr("class") + ":last").remove();
 }
 
 
@@ -20,9 +32,64 @@ const color = '#'+Math.floor(Math.random()*16777215).toString(16);
 return color;
 }
 
+const darkenColor = (color, amt) => {
+  let usePound = false;
+  if(color[0] == "#") {
+    color = color.slice(1);
+    usePound = true;
+  }
+  let num = parseInt(color, 16);
+  
+  let r = (num >> 16) + amt;
+  if (r > 255) r = 255;
+  else if (r < 0) r = 0;
+
+  let b = ((num >> 8) & 0x00FF) + amt;
+ 
+    if (b > 255) b = 255;
+    else if  (b < 0) b = 0;
+ 
+  let g = (num & 0x0000FF) + amt;
+ 
+    if (g > 255) g = 255;
+    else if (g < 0) g = 0;
+ 
+    return (usePound?"#":"") + (g | (b << 8) | (r << 16)).toString(16);
+
+}
+
+// function LightenDarkenColor(col, amt) {
+  
+//     var usePound = false;
+  
+//     if (col[0] == "#") {
+//         col = col.slice(1);
+//         usePound = true;
+//     }
+ 
+//     var num = parseInt(col,16);
+ 
+//     var r = (num >> 16) + amt;
+ 
+//     if (r > 255) r = 255;
+//     else if  (r < 0) r = 0;
+ 
+//     var b = ((num >> 8) & 0x00FF) + amt;
+ 
+//     if (b > 255) b = 255;
+//     else if  (b < 0) b = 0;
+ 
+//     var g = (num & 0x0000FF) + amt;
+ 
+//     if (g > 255) g = 255;
+//     else if (g < 0) g = 0;
+ 
+//     return (usePound?"#":"") + (g | (b << 8) | (r << 16)).toString(16);
+  
+// }
+
 const toggleFavorite = (event) => {
   $(event.target).toggleClass('lock-icon')
-  // $(event.target).toggleClass('unlock-icon')
   $(event.target).parents('.color-box').toggleClass('favorite');
 }
 
---
+++ server.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const path = require('path');
+
+const environment = process.env.NODE_ENV || 'development';
+const configuration = require('./knexfile')[environment];
+const database = require('knex')(configuration);
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(path.join(__dirname + '/public')));
+
+app.locals.title = 'Palette Picker';
+
+app.set('port', process.env.PORT || 3000);
+
+app.get('/', (request, response) => {
+  response.send('Welcome to Palette Picker');
+});
+
+app.listen(app.get('port'), () => {
+  console.log(`${app.locals.title} is running on ${app.get('port')}.`);
+});
+
+app.get('/api/v1/projects', (request, response) =>{
+  database('projects').select()
+  .then((projects) => {
+    response.status(200).json({projects});
+  })
+  .catch((error) => {
+    response.status(500).json({error})
+  })
+});
+
+app.get('/api/v1/palettes', (request, response) =>{
+  database('palettes').select()
+  .then((palettes) => {
+    response.status(200).json({palettes});
+  })
+  .catch((error) => {
+    response.status(500).json({error})
+  })
+})
