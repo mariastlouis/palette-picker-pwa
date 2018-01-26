d55c7e2b1bc7298aac8ab3ff5581f72eb6d8a453
mariastlouis <msantray@yahoo.com>
2018-01-26 10:55:29 -0700

    knex file added with projection

--- knexfile.js
+++ knexfile.js
@@ -23,5 +23,13 @@ module.exports = {
     seeds: {
       directory: './db/seeds/test'
     }    
-  }
+  },
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL + `?ssl=true`,
+    migrations: {
+    directory: './db/migrations'
+  },
+  useNullAsDefault: true
+}
 };
